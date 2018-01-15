b0a72693435bed05c78bfb55581a139acd5b855e
zyc <zyc@zyc.local>
2018-01-16 00:52:33 +0800

    init version

---
+++ texcount.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+
+import os,sys
+
+def readTex(fileName):
+    curPath=os.path.dirname(fileName)
+    lineArray=[]
+    with open(fileName,'r') as f:
+        lines=f.readlines()
+        for line in lines:
+            line=line.strip()
+            if '\include' in line or '\input' in line:
+                fileName=line.split("{")[1][0:-1]+".tex"
+                lineArray.extend(readTex(os.path.join(curPath,fileName)))
+            else:
+                lineArray.append(line)
+    return lineArray
+    
+def count(lineArray):
+    count=0
+    for line in lineArray:
+        if not line.startswith("\\"):
+            count+=len(line.split(" "))
+    return count
+
+if __name__ == '__main__':
+    texFile=sys.argv[1]
+    print count(readTex(texFile))
